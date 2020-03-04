d8da9b584795db504e4efae92ffe33c3c916ba6e
Kai <kaiyip.ho23@gmail.com>
2020-03-03 22:51:58 -0500

    small changes

--- js/game_logic.js
+++ js/game_logic.js
@@ -3,22 +3,60 @@ const COLORS = ["red","green", "blue", "yellow"]
 let Kai = {
   sequence: [],
   playerInput: [],
+  gameOn: true,
   gameOver: false,
   defaultLevel: "Ash",
   level: 1
 }
 
-  startButton = document.getElementById("start");
-  startButton.addEventListener('click', function(e){
-    // console.log("START")
-  })
+// grabbing all the html elements
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const blue = document.getElementById("blue");
+const green = document.getElementById("green");
 
+//event listeners for click to get player input
+const redInput = red.addEventListener( "click", (e) => {
+  if (Kai.gameOn){
+    Kai.playerInput.push("red")
+  }
+})
+
+const yellowInput = yellow.addEventListener("click", e => {
+  if (Kai.gameOn) {
+    Kai.playerInput.push("yellow");
+  }
+});
+
+const blueInput = blue.addEventListener("click", e => {
+  if (Kai.gameOn) {
+    Kai.playerInput.push("blue");
+  }
+});
+
+const greenInput = green.addEventListener("click", e => {
+  if (Kai.gameOn) {
+    Kai.playerInput.push("green");
+  }
+});
+
+//generates the sequence for this round
 const sequenceGenerator = () => {
-  randomIndex = Math.floor((Math.random() * 4))
-  Kai.sequence.push(COLORS[randomIndex])
+  for (let i = 0; i < 15; i++) {
+    randomIndex = Math.floor((Math.random() * 4))
+    Kai.sequence.push(COLORS[randomIndex])
+  }
   // return Kai
 }
 
+startButton = document.getElementById("start");
+startButton.addEventListener('click', function(e){
+  console.log("START")
+})
+
+const play = () => {
+  
+}
 const colorRelay = () => {
   for (let i = 0; i < Kai.sequence.length; i++) {
     button = document.getElementById(`${Kai.sequence[i]}`);
@@ -26,11 +64,9 @@ const colorRelay = () => {
   }
 }
 
-// const gameOver = () => {
-//   if (Kai.gameOver){
-    
-//   }
-// }
+const checker = () => {
+  
+}
 
 const resetGame = () => {
   Kai = {
